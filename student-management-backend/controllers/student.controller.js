e694ee8510ba7243cef5be314a422b6853ac458a
patan subahan <subahan@infasta.com>
2025-12-29 13:17:26 +0530

    second modify changes

--- student-management-backend/controllers/student.controller.js
+++ student-management-backend/controllers/student.controller.js
@@ -1,41 +1,65 @@
 const Student = require('../models/student.model');
 
 // GET all students
+// exports.getStudents = async (req, res) => {
+//     const students = await Student.find();
+//     res.json(students);
+// };
+
+// GET students with pagination
 exports.getStudents = async (req, res) => {
-    const students = await Student.find();
-    res.json(students);
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const sortField = req.query.sort || 'createdAt';
+    const sortOrder = req.query.order === 'desc' ? -1 : 1;
+    const skip = (page - 1) * limit;
+
+    const total = await Student.countDocuments();
+
+    const students = await Student.find()
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortField]: sortOrder });
+
+    res.json({
+      data: students,
+      total,
+      page,
+      limit
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching students' });
+  }
 };
 
-// GET student by ID
+
 exports.getStudentById = async (req, res) => {
-    const student = await Student.findById(req.params.id);
-    res.json(student);
+  const student = await Student.findById(req.params.id);
+  res.json(student);
 };
 
-// ADD student
 exports.addStudent = async (req, res) => {
   try {
     const student = new Student(req.body);
     await student.save();
-    res.status(201).json(student); 
+    res.status(201).json(student);
   } catch (err) {
     res.status(500).json({ message: 'Error adding student', error: err });
   }
 };
 
 
-// UPDATE student
 exports.updateStudent = async (req, res) => {
-    const student = await Student.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true }
-    );
-    res.json(student);
+  const student = await Student.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.json(student);
 };
 
-// DELETE student
 exports.deleteStudent = async (req, res) => {
-    await Student.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Student deleted successfully' });
+  await Student.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Student deleted successfully' });
 };
